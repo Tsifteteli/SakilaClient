8e034a3fccdb85626f2b1f85a3f4373f7d34e981
Tsifteteli <46268137+Tsifteteli@users.noreply.github.com>
2020-01-07 11:01:52 +0100

    Allt

---
+++ src/main/java/com/mycompany/sakilaclient/Person.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sakilaclient;
+
+/**
+ *
+ * @author Caroline
+ */
+public class Person {
+   
+   int id;
+   String first_name;
+   String last_name;
+
+   public int getId() {
+      return id;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public String getFirst_name() {
+      return first_name;
+   }
+
+   public void setFirst_name(String first_name) {
+      this.first_name = first_name;
+   }
+
+   public String getLast_name() {
+      return last_name;
+   }
+
+   public void setLast_name(String last_name) {
+      this.last_name = last_name;
+   }
+   
+}
